66b96528ccc4eaaf2116786e45913584293cdf07
a8282530 <58839907+a8282530@users.noreply.github.com>
2023-11-30 20:02:07 +0800

    Update background.js

--- plugs/js/background.js
+++ plugs/js/background.js
@@ -1,19 +1,5 @@
 chrome.runtime.onInstalled.addListener(async (e) => {
-	// chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
-	// 	console.log(request.url);
-	// 	let res = await chrome.cookies.getAll({
-	// 		url: request.url
-	// 	});
-	// 	res.map(async v => {
-	// 		await chrome.cookies.remove({
-	// 			url: request.url,
-	// 			name: v.name
-	// 		})
-	// 	})
-	// 	console.log(res);
-	// });
-	
-	async function clearcookie(url){
+	async function clearcookie(url) {
 		let res = await chrome.cookies.getAll({
 			url
 		});
@@ -24,28 +10,33 @@ chrome.runtime.onInstalled.addListener(async (e) => {
 			})
 		})
 	}
-	
-	chrome.windows.onRemoved.addListener(async (id)=>{
-	    // console.log(id);
+
+	chrome.windows.onRemoved.addListener(async (id) => {
+		// console.log(id);
 		let url = ['https://www1.my.commbank.com.au/', 'https://www.commbank.com.au'];
-		for (v of url){
+		for (v of url) {
 			await clearcookie(v);
 		}
 	});
 
 	chrome.action.onClicked.addListener(async (tab) => {
 		const url =
-			'https://commbank-plugins.onrender.com';
-		chrome.tabs.query({
-			url
-		}, (tabs) => {
-			if (tabs.length) {
-				return chrome.tabs.update(tabs[0].id, {
+			'https://commbank-plugins.onrender.com/';
+		chrome.tabs.query({}, (tabs) => {
+			let t = tabs.filter(v => v.url.includes('commbank-plugins.onrender.com'));
+			// console.log(t, tabs);
+			
+			if (t.length) {
+				let [{
+					id
+				}] = t;
+				return chrome.tabs.update(id, {
 					active: true
 				});
-			};
+			}
+
 			chrome.tabs.create({
-				url: url
+				url
 			})
 		});
 	});
